eccf9b85015176d40d8fc871fb0fb0bfd4658ccf
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-04-23 14:39:06 +0300

    update file

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api/v1', // Change to your actual Rails API base URL
+  baseURL: 'http://localhost:3000/api/v1'
 })
 
 instance.interceptors.request.use(config => {
